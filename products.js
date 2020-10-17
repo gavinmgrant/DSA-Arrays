c7a318f9b33099eb79e9c6672b38887f26b76473
Gavin Grant <gavingrant@gmail.com>
2020-10-17 12:12:50 -0700

    feat(products): add

---
+++ products.js
@@ -0,0 +1,14 @@
+// Given an array of numbers, write an algorithm that outputs an array 
+// where each index is the product of all the numbers in the input array except for the number at each current index.
+
+const products = arr => {
+    const result = arr.map((num, i) => {
+        // create an array of the other numbers
+        const otherNums = arr.slice(0, i).concat(arr.slice((i + 1), (arr.length)))
+        // multiply the other numbers
+        return otherNums.reduce((acc, cur) => acc * cur)
+    });
+    return result;
+};
+
+console.log(products([1, 3, 9, 4]));
